47205cffcd85878961a36d3d798fc07e3bb99529
Walber Felyppi Santos Sousa <felyppiss@gmail.com>
2021-02-28 17:22:28 -0300

    Inclusão de mensagens no aplicativo e opção para selecionar a melhor opção possível

--- Download.py
+++ Download.py
@@ -1,14 +1,15 @@
-from pytube            import YouTube, Playlist
+from pytube  import YouTube, Playlist
+from getpass import getuser
 #from pytube.exceptions import *
 
 
-class download:
-    pasta      = "Download"
-    conclusao  = "Arquivo salvo na pasta Download! \n\n"
-    exception  = "Existe algum vídeo indisponível, o mesmo não pôde ser baixado! \nContinuando o processo, aguarde...\n"
-    regexmatch = "Link digitado incorreto"
-                
 
+class download:
+    #desktop   = "C:/Users/" + getuser() + "/Desktop"
+    desktop   = "Download"
+    conclusao = "Arquivo salvo na pasta Download! \n\n"
+    excecao   = "Existe algum vídeo indisponível, o mesmo não pôde ser baixado! \nContinuando o processo, aguarde...\n"
+                
 
     
     def __init__(self):
@@ -18,29 +19,29 @@ class download:
     # Baixa vídeo
     def video(self, link):
         yt = YouTube(link)
-        yt.streams.first().download(self.pasta)
+        yt.streams.get_highest_resolution().download(self.desktop)
         return print (self.conclusao)
     
 
 
     # Baixa playlist de vídeos
     def pl_video(self, link):
-        playlist = Playlist(link)
-        for url in playlist.video_urls:
+        pl = Playlist(link)
+        for url in pl.video_urls:
             try:
-                video = YouTube(url)
+                yt = YouTube(url)
             except Exception:
                 print (self.excecao)
             else:
-                video.streams.first().download(self.pasta)
+                yt.streams.get_highest_resolution().download(self.desktop)
         return print (self.conclusao)
         
 
         
     # Baixa como áudio
     def audio(self, link):
-        video = YouTube(link)
-        video.streams.filter(only_audio = True).first().download(self.pasta)
+        audio = YouTube(link)
+        audio.streams.filter(only_audio = True).filter(file_extension = "mp4").first().download(self.desktop)
         return print (self.conclusao)
             
         
@@ -55,7 +56,7 @@ class download:
                 print (self.excecao)
                 pass
             else:
-                audio.streams.filter(only_audio = True).first().download(self.pasta)
+                audio.streams.filter(only_audio = True).filter(file_extension = "mp4").first().download(self.desktop)
         return print (self.conclusao)
         
 ## FIM DA APLICAÇÃO ##
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Download import download #Classe contendo a implementação da biblioteca PyTube
 from pytube.exceptions import RegexMatchError
-#import getpass               #Captura o usuário logado
+
 
 print ("#### Script para download de vídeos ou músicas do YouTube ####")
 print ("####        Autor: Walber Felyppi Santos Sousa            ####")
@@ -8,22 +8,24 @@ print ("####            E-mail: felyppiss@gmail.com               #### \n\n")
 
 while(True): #Mantém o programa rodando indefinidamente
     try:
-        opcao = input("Seleciona uma das opções a seguir:\n1: Vídeo   2: Playlist vídeo   3: Áudio   4: Playlist áudio\n")
-        link  = input("Insira o link do video ou playlist: \n")
-
-        #usuario = getpass.getuser()
-        #local   = "C:/Users/" + usuario + "/Desktop"
+        opcao    = input("Seleciona uma das opções a seguir:\n1: Vídeo   2: Playlist vídeo   3: Áudio   4: Playlist áudio\n")
+        link     = input("Insira o link do video ou playlist: \n")
+        baixando = "Iniciando o download...\n"
  
         if  (opcao == '1'):
+            print (baixando)
             download().video    (link)
 
         elif (opcao == '2'):
+            print (baixando)    
             download().pl_video (link)
 
         elif (opcao == '3'):
+            print (baixando)
             download().audio    (link)
 
         elif (opcao == '4'):
+            print (baixando)
             download().pl_audio (link)
 
     except RegexMatchError:
