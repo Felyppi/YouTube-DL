05a55d50c11711192f3da502d1231f3aae63ad87
Walber Felyppi Santos Sousa <felyppiss@gmail.com>
2021-02-27 23:21:30 -0300

    Refatoração de código

--- Download.py
+++ Download.py
@@ -1,20 +1,26 @@
 from pytube            import YouTube, Playlist
-from pytube.exceptions import *
+#from pytube.exceptions import *
 
 
-class download:    
+class download:
+    pasta      = "Download"
+    conclusao  = "Arquivo salvo na pasta Download! \n\n"
+    exception  = "Existe algum vídeo indisponível, o mesmo não pôde ser baixado! \nContinuando o processo, aguarde...\n"
+    regexmatch = "Link digitado incorreto"
+                
+
+
+    
     def __init__(self):
         return
 
     
-
     # Baixa vídeo
     def video(self, link):
-        video = YouTube(link)
-        video.streams.first().download('Download')
-        return print ("Arquivo salvo em Download! \n\n\n")
-        
-
+        yt = YouTube(link)
+        yt.streams.first().download(self.pasta)
+        return print (self.conclusao)
+    
 
 
     # Baixa playlist de vídeos
@@ -23,31 +29,22 @@ class download:
         for url in playlist.video_urls:
             try:
                 video = YouTube(url)
-            except (Exception):
-                print ('Existe algum vídeo indisponível, o mesmo não pôde ser baixado! \n')
-                print ('Continuando o processo, aguarde...\n')
-                pass
+            except Exception:
+                print (self.excecao)
             else:
-                video.streams.first().download('Download')
-        return print ("Arquivo salvo em Download! \n\n\n")
+                video.streams.first().download(self.pasta)
+        return print (self.conclusao)
         
-
-
 
         
     # Baixa como áudio
     def audio(self, link):
         video = YouTube(link)
-        video.streams.filter(only_audio = True).first().download('Download')
-        return print ("Arquivo salvo em Download! \n\n\n")
+        video.streams.filter(only_audio = True).first().download(self.pasta)
+        return print (self.conclusao)
             
         
 
-        
-
-
-        
-               
     # Baixa playlist de áudio
     def pl_audio(self, link):
         playlist = Playlist(link)
@@ -55,11 +52,10 @@ class download:
             try:
                 audio = YouTube(url)
             except Exception:
-                print ('Existe algum link indisponível, o mesmo não pôde ser baixado! \n')
-                print ('Continuando o processo, aguarde...\n')
+                print (self.excecao)
                 pass
             else:
-                audio.streams.filter(only_audio = True).first().download('Download')
-        return print ("àudios salvos em Download! \n\n\n")
+                audio.streams.filter(only_audio = True).first().download(self.pasta)
+        return print (self.conclusao)
         
 ## FIM DA APLICAÇÃO ##
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from Download import download # Classe contendo a implementação da biblioteca PyTube
-import getpass                # Captura o usuário logado
+from Download import download #Classe contendo a implementação da biblioteca PyTube
+from pytube.exceptions import RegexMatchError
+#import getpass               #Captura o usuário logado
 
 print ("#### Script para download de vídeos ou músicas do YouTube ####")
-print ("        ####  Por: Walber Felyppi Santos Sousa  ####")
-print ("        ####  E-mail: felyppiss@gmail.com       #### \n \n")
+print ("####        Autor: Walber Felyppi Santos Sousa            ####")
+print ("####            E-mail: felyppiss@gmail.com               #### \n\n")
 
-while(True): # Mantém o programa rodando indefinidamente
+while(True): #Mantém o programa rodando indefinidamente
     try:
-        opcao = input("1: Vídeo   2: Playlist vídeo   3: Áudio   4: Playlist áudio \n")
+        opcao = input("Seleciona uma das opções a seguir:\n1: Vídeo   2: Playlist vídeo   3: Áudio   4: Playlist áudio\n")
         link  = input("Insira o link do video ou playlist: \n")
+
         #usuario = getpass.getuser()
         #local   = "C:/Users/" + usuario + "/Desktop"
  
-
         if  (opcao == '1'):
             download().video    (link)
 
@@ -25,11 +26,14 @@ while(True): # Mantém o programa rodando indefinidamente
         elif (opcao == '4'):
             download().pl_audio (link)
 
-
-    except KeyboardInterrupt:
-        print ('Processo reiniciado ! \n')
+    except RegexMatchError:
+        print ("\nInforme o link correto!\n\n")
         pass
     
-    #except BaseException as err:
-     #   print ('Erro interno em main.py! \n', err)
-      #  pass
+    except KeyboardInterrupt:
+        print ('\nProcesso reiniciado!\n\n')
+        pass
+    
+    except BaseException as err:
+        print ('\nErro interno em main.py!', err, '\n\n')
+        pass
